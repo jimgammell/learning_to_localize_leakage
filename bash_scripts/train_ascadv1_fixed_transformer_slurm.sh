f6f83817eae7ac9eea4759a7a2ce5207b67f2d90
Jim Gammell <jgammell@purdue.edu>
2026-02-08 10:00:52 -0500

    ...

---
+++ bash_scripts/train_ascadv1_fixed_transformer_slurm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --job-name=train-ascadv1f-transformer
+#SBATCH --partition=cocosys
+#SBATCH --account=cocosys
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-gpu=14
+#SBATCH --time=24:00:00
+#SBATCH --output=train-ascadv1f-transformer-%a.out
+#SBATCH --error=train-ascadv1f-transformer-%a.out
+#SBATCH --array=0-7
+
+LR=(7e-5 1e-4 3e-4 5e-4)
+ROLL=(0 10)
+
+IDX=$SLURM_ARRAY_TASK_ID
+
+source ~/.bashrc
+micromamba activate leakage-localization
+python -m experiments.train.supervised \
+    --dest=./outputs/ascadv1_fixed/supervised_reg_${NAMES[$IDX]} \
+    --config-file=ascadv1_fixed_transformer \
+    --training.base_lr ${LR[$IDX]} \
+    --data.random_roll ${ROLL[$IDX]} 

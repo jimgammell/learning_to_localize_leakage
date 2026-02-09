b6c9e040af1acb4159b48bfbb681367413b08d52
Jim Gammell <jgammell@purdue.edu>
2026-02-09 14:35:28 -0500

    fixed attention pooling implementation

--- src/leakage_localization/models/building_blocks/pooling.py
+++ src/leakage_localization/models/building_blocks/pooling.py
@@ -3,7 +3,7 @@ from typing import Optional
 import torch
 from torch import nn
 
-from .blocks import CrossAttentionBlock
+from .attention import CrossAttention
 
 class AveragePool(nn.Module):
     def __init__(
@@ -33,20 +33,22 @@ class TokenPool(nn.Module):
         assert x.shape[1] >= self.output_tokens
         return x[:, -self.output_tokens:, :]
 
-class AttentionPool(CrossAttentionBlock):
+class AttentionPool(nn.Module):
     def __init__(
             self,
             *,
             output_tokens: int,
+            embedding_dim: int,
             **kwargs
     ):
-        kwargs['use_rope'] = False
-        super().__init__(**kwargs)
-
-        self.pre_query = nn.Parameter(torch.empty((output_tokens, kwargs['embedding_dim'])))
+        super().__init__()
+        self.attn_input_norm = nn.LayerNorm(embedding_dim, eps=1e-5, elementwise_affine=True, bias=True)
+        self.attn_context_norm = nn.LayerNorm(embedding_dim, eps=1e-5, elementwise_affine=True, bias=True)
+        self.cross_attn = CrossAttention(embedding_dim=embedding_dim, **kwargs)
+        self.pre_query = nn.Parameter(torch.empty((output_tokens, embedding_dim)))
         nn.init.trunc_normal_(self.pre_query, std=0.02)
     
     def forward(self, x: torch.Tensor, attn_mask: Optional[torch.Tensor] = None) -> torch.Tensor:
         batch_size, *_ = x.shape
         pre_query = self.pre_query.unsqueeze(0).expand(batch_size, -1, -1)
-        return super().forward(pre_query, x, context_mask=attn_mask)
+        return self.cross_attn(self.attn_input_norm(pre_query), context=self.attn_context_norm(x), context_mask=attn_mask)

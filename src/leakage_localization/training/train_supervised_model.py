a2a4725c82dea0cba17c249125167256551f277a
Jim Gammell <jgammell@purdue.edu>
2026-02-21 11:29:56 -0500

    ...

--- src/leakage_localization/training/train_supervised_model.py
+++ src/leakage_localization/training/train_supervised_model.py
@@ -55,8 +55,8 @@ def train_supervised_model(
         gradient_clip_val=grad_clip_val,
         default_root_dir=dest,
     )
-    trainer.fit(training_module, train_dataloaders=train_loader, val_dataloaders=val_loader, ckpt_path=dest / f'{best_ckpt_name}.ckpt')
+    trainer.fit(training_module, train_dataloaders=train_loader, val_dataloaders=val_loader)
     try:
-        trainer.test(training_module, dataloaders=test_loader)
+        trainer.test(training_module, dataloaders=test_loader, ckpt_path=dest / f'{best_ckpt_name}.ckpt')
     except Exception as e:
         print(f"Warning: test step failed with {type(e).__name__}: {e}")

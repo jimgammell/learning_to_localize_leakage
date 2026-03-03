585da649f54047f107cc287cac4dbbd68d475dc4
Jim Gammell <jgammell@purdue.edu>
2026-03-03 17:27:38 -0500

    ...

--- slurm_scripts/htune_ascadv1f.sh
+++ slurm_scripts/htune_ascadv1f.sh
@@ -16,4 +16,4 @@ python -m experiments.train.supervised \
     --dest ./outputs/ascadv1_fixed/htune \
     --config-file ascadv1_fixed \
     --optuna-study-path ./outputs/ascadv1_fixed/htune/optuna_study.log
-    --optuna-run-count 48
+    --optuna-run-count 96

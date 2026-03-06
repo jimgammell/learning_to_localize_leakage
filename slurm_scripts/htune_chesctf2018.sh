6005a09758e37dfdb204ee8253c82080ad11dcf9
Jim Gammell <jgammell@purdue.edu>
2026-03-06 17:35:10 -0500

    ...

--- slurm_scripts/htune_chesctf2018.sh
+++ slurm_scripts/htune_chesctf2018.sh
@@ -6,8 +6,8 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-gpu=14
 #SBATCH --time=12:00:00
-#SBATCH --output=./outputs/ches_ctf_2018/htune/slurm_%A_%a.out
-#SBATCH --error=./outputs/ches_ctf_2018/htune/slurm_%A_%a.out
+#SBATCH --output=./outputs/ches_ctf_2018/reg_htune/slurm_%A_%a.out
+#SBATCH --error=./outputs/ches_ctf_2018/reg_htune/slurm_%A_%a.out
 #SBATCH --array=0-999%8
 
 source ~/.bashrc

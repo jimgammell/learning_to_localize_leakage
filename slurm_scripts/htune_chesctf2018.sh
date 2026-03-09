a430e4a9fce0d1bb8d26e189062a19a4afe747f9
Jim Gammell <jgammell@purdue.edu>
2026-03-09 10:06:30 -0400

    ...

--- slurm_scripts/htune_chesctf2018.sh
+++ slurm_scripts/htune_chesctf2018.sh
@@ -8,7 +8,7 @@
 #SBATCH --time=12:00:00
 #SBATCH --output=./outputs/ches_ctf_2018/reg_htune/slurm_%A_%a.out
 #SBATCH --error=./outputs/ches_ctf_2018/reg_htune/slurm_%A_%a.out
-#SBATCH --array=0-999%8
+#SBATCH --array=0-999%2
 
 source ~/.bashrc
 micromamba activate leakage-localization
@@ -16,4 +16,4 @@ python -m experiments.train.supervised \
     --dest ./outputs/ches_ctf_2018/reg_htune \
     --config-file ches_ctf_2018 \
     --optuna-study-path ./outputs/ches_ctf_2018/reg_htune/optuna_study.log \
-    --sampler qmc
+    --sampler tpe

65aa79484a31e86d4f81c4d36bedebfb316cda7b
Jim Gammell <jgammell@purdue.edu>
2026-03-09 10:35:36 -0400

    ...

--- slurm_scripts/htune_dpav4d2.sh
+++ slurm_scripts/htune_dpav4d2.sh
@@ -6,13 +6,14 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-gpu=14
 #SBATCH --time=12:00:00
-#SBATCH --output=./outputs/dpav4_2/htune/slurm_%A_%a.out
-#SBATCH --error=./outputs/dpav4_2/htune/slurm_%A_%a.out
-#SBATCH --array=0-999%8
+#SBATCH --output=./outputs/dpav4_2/reg_htune/slurm_%A_%a.out
+#SBATCH --error=./outputs/dpav4_2/reg_htune/slurm_%A_%a.out
+#SBATCH --array=0-999%2
 
 source ~/.bashrc
 micromamba activate leakage-localization
 python -m experiments.train.supervised \
-    --dest ./outputs/dpav4_2/htune \
+    --dest ./outputs/dpav4_2/reg_htune \
     --config-file dpav4_2 \
-    --optuna-study-path ./outputs/dpav4_2/htune/optuna_study.log
+    --optuna-study-path ./outputs/dpav4_2/reg_htune/optuna_study.log \
+    --sampler qmc

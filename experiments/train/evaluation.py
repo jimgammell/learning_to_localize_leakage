3b85c0786c897b117014d9a2e455255eecaf31d8
Jim Gammell <jgammell@purdue.edu>
2026-03-10 16:36:05 -0400

    ...

--- experiments/train/evaluation.py
+++ experiments/train/evaluation.py
@@ -7,6 +7,7 @@ from numpy.typing import NDArray
 from lightning import Trainer
 from matplotlib import pyplot as plt
 import pandas
+from torch.utils.data import Dataset
 
 from experiments.initialization import *
 from experiments.initialization.directories import safe_load_yaml
@@ -15,9 +16,26 @@ from leakage_localization.training.supervised_lightning_module import Supervised
 
 def load_training_module(
         ckpt_path: Path,
-        trace_statistics: Dict[str, NDArray[np.floating]]
+        profiling_set: Dataset,
+        trace_statistics: Dict[str, NDArray[np.floating]],
+        config: Dict[str, Any]
 ) -> SupervisedModule:
-    module = SupervisedModule.load_from_checkpoint(ckpt_path, map_location='cpu', weights_only=False, trace_statistics=trace_statistics)
+    module = SupervisedModule.load_from_checkpoint(
+        ckpt_path,
+        map_location='cpu',
+        weights_only=False,
+        trace_statistics=trace_statistics,
+        mtd_kwargs={
+            'target_preds_to_key_preds': profiling_set.target_preds_to_key_preds,
+            'int_var_keys': profiling_set.int_var_keys,
+            'attack_count': config['mtd']['attack_count'],
+            'traces_per_attack': config['mtd']['traces_per_attack']
+        },
+        mixup_alpha=config['training'].get('mixup_alpha', 0.0),
+        random_roll_scale=float(config['data'].get('random_roll_scale', 0.0)),
+        random_lpf_scale=float(config['data'].get('random_lpf_scale', 0.0)),
+        compute_val_mtd=config['training'].get('early_stop_metric', 'val/rank') == 'val/mtd',
+    )
     return module
 
 def plot_training_curves(
@@ -71,9 +89,9 @@ def main():
     with open(config_path, 'r') as f:
         config = safe_load_yaml(f)
     
-    train_set, val_set, test_set, trace_statistics = construct_datasets(config)
+    profiling_set, attack_set, train_set, val_set, test_set, trace_statistics = construct_datasets(config, val_partition='profile')
     train_loader, val_loader, test_loader = construct_loaders(train_set, val_set, test_set, config)
-    module = load_training_module(ckpt_path, trace_statistics=trace_statistics)
+    module = load_training_module(ckpt_path, profiling_set=profiling_set, trace_statistics=trace_statistics, config=config)
     trainer = Trainer(
         accelerator='gpu',
         precision='bf16-mixed',

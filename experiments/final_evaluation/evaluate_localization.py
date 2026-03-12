a6c4cc2ba35e2852696c5135042852d0cffd5cb8
Jim Gammell <jgammell@purdue.edu>
2026-03-12 18:21:33 -0400

    ...

--- experiments/final_evaluation/evaluate_localization.py
+++ experiments/final_evaluation/evaluate_localization.py
@@ -11,6 +11,10 @@ from leakage_localization.datasets.common import PARTITION
 
 DATASET = Literal['ascadv1-fixed']
 
+def _annotate_spearman(ax, x, y):
+    rho = spearmanr(x, y).correlation
+    ax.text(0.95, 0.95, f'ρ={rho:.3f}', transform=ax.transAxes, ha='right', va='top', fontsize=6)
+
 def visualize_ascadv1_snr(base_dir: Path, partition: PARTITION = 'attack'):
     snr_dir = base_dir / 'snr'
     snr_vals = dict()
@@ -28,7 +32,7 @@ def visualize_ascadv1_snr(base_dir: Path, partition: PARTITION = 'attack'):
         gradvis_path = subdir / 'gradvis.npy'
         gradvis = np.load(gradvis_path)
 
-        fig, axes = plt.subplots(5, 16, figsize=(2*16, 2*3), sharex=True, sharey=True)
+        fig, axes = plt.subplots(5, 16, figsize=(2*16, 2*5), sharex=True, sharey=True)
         for ax in axes.flatten():
             ax.set_xscale('log')
             ax.set_yscale('log')
@@ -46,32 +50,30 @@ def visualize_ascadv1_snr(base_dir: Path, partition: PARTITION = 'attack'):
             rasterized=True
         )
         for byte_idx in range(16):
-            full_axes[byte_idx].plot(
-                0.25*(
+            if byte_idx >= 2:
+                full_x = 0.25*(
                     snr_vals['subbytes'][byte_idx, :]
                     + 0.5*(snr_vals['r_in'][0, :] + snr_vals['p__xor__k__xor__r_in'][byte_idx, :])
                     + 0.5*(snr_vals['r'][byte_idx, :] + snr_vals['subbytes__xor__r'][byte_idx, :])
                     + 0.5*(snr_vals['r_out'][0, :] + snr_vals['subbytes__xor__r_out'][byte_idx, :])
-                ),
-                gradvis[byte_idx, :],
-                **plot_kwargs
-            )
-            subbytes_axes[byte_idx].plot(snr_vals['subbytes'][byte_idx, :], gradvis[byte_idx, :], **plot_kwargs)
-            rin_axes[byte_idx].plot(
-                0.5*(snr_vals['r_in'][0, :] + snr_vals['p__xor__k__xor__r_in'][byte_idx, :]),
-                gradvis[byte_idx, :],
-                **plot_kwargs
-            )
-            r_axes[byte_idx].plot(
-                0.5*(snr_vals['r'][byte_idx, :] + snr_vals['subbytes__xor__r'][byte_idx, :]),
-                gradvis[byte_idx, :],
-                **plot_kwargs
-            )
-            rout_axes[byte_idx].plot(
-                0.5*(snr_vals['r_out'][0, :] + snr_vals['subbytes__xor__r_out'][byte_idx, :]),
-                gradvis[byte_idx, :],
-                **plot_kwargs
-            )
+                )
+                full_axes[byte_idx].plot(full_x, gradvis[byte_idx, :], **plot_kwargs)
+                _annotate_spearman(full_axes[byte_idx], full_x, gradvis[byte_idx, :])
+            else:
+                full_axes[byte_idx].plot(snr_vals['subbytes'][byte_idx, :], gradvis[byte_idx, :], **plot_kwargs)
+                _annotate_spearman(full_axes[byte_idx], snr_vals['subbytes'][byte_idx, :], gradvis[byte_idx, :])
+            subbytes_x = snr_vals['subbytes'][byte_idx, :]
+            subbytes_axes[byte_idx].plot(subbytes_x, gradvis[byte_idx, :], **plot_kwargs)
+            _annotate_spearman(subbytes_axes[byte_idx], subbytes_x, gradvis[byte_idx, :])
+            rin_x = 0.5*(snr_vals['r_in'][0, :] + snr_vals['p__xor__k__xor__r_in'][byte_idx, :])
+            rin_axes[byte_idx].plot(rin_x, gradvis[byte_idx, :], **plot_kwargs)
+            _annotate_spearman(rin_axes[byte_idx], rin_x, gradvis[byte_idx, :])
+            r_x = 0.5*(snr_vals['r'][byte_idx, :] + snr_vals['subbytes__xor__r'][byte_idx, :])
+            r_axes[byte_idx].plot(r_x, gradvis[byte_idx, :], **plot_kwargs)
+            _annotate_spearman(r_axes[byte_idx], r_x, gradvis[byte_idx, :])
+            rout_x = 0.5*(snr_vals['r_out'][0, :] + snr_vals['subbytes__xor__r_out'][byte_idx, :])
+            rout_axes[byte_idx].plot(rout_x, gradvis[byte_idx, :], **plot_kwargs)
+            _annotate_spearman(rout_axes[byte_idx], rout_x, gradvis[byte_idx, :])
         fig.tight_layout()
         fig.savefig(subdir / 'gradvis_vs_snr.pdf')
         plt.close(fig)
@@ -79,12 +81,14 @@ def visualize_ascadv1_snr(base_dir: Path, partition: PARTITION = 'attack'):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', action='store', choices=get_args(DATASET), required=True)
+    parser.add_argument('--partition', action='store', choices=get_args(PARTITION), default='attack')
     args = parser.parse_args()
 
     dataset: DATASET = args.dataset
+    partition: PARTITION = args.partition
     if dataset == 'ascadv1-fixed':
         output_dir = OUTPUTS_ROOT / 'ascadv1_fixed'
-        visualize_ascadv1_snr(output_dir, 'attack')
+        visualize_ascadv1_snr(output_dir, partition)
 
 if __name__ == '__main__':
     main()

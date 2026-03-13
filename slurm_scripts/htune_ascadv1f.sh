8e5218cc1c767c75bc995654654b2ba7c02bff1e
Jim Gammell <jgammell@purdue.edu>
2026-03-13 12:31:55 -0400

    ...

--- slurm_scripts/htune_ascadv1f.sh
+++ slurm_scripts/htune_ascadv1f.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-#SBATCH --job-name=htune-ascadv1f
+#SBATCH --job-name=ascadv1f-unreg-htune
 #SBATCH --partition=cocosys
 #SBATCH --account=cocosys
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-gpu=14
 #SBATCH --time=4:00:00
-#SBATCH --output=./outputs/ascadv1_fixed/htune/slurm_%A_%a.out
-#SBATCH --error=./outputs/ascadv1_fixed/htune/slurm_%A_%a.out
+#SBATCH --output=./outputs/ascadv1_fixed/unreg_htune/slurm_%A_%a.out
+#SBATCH --error=./outputs/ascadv1_fixed/unreg_htune/slurm_%A_%a.out
 #SBATCH --array=0-999%2
 
 source ~/.bashrc
 micromamba activate leakage-localization
 python -m experiments.train.supervised \
-    --dest ./outputs/ascadv1_fixed/htune \
+    --dest ./outputs/ascadv1_fixed/unreg_htune \
     --config-file ascadv1_fixed \
-    --optuna-study-path ./outputs/ascadv1_fixed/htune/optuna_study.log \
+    --optuna-study-path ./outputs/ascadv1_fixed/unreg_htune/optuna_study.log \
     --sampler qmc

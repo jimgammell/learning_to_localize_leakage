f07ae558817a46cdb472a6f869388e017957c70f
Jim Gammell <jgammell@purdue.edu>
2026-03-17 14:40:41 -0400

    ...

--- slurm_scripts/htune_dpav4d2.sh
+++ slurm_scripts/htune_dpav4d2.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-#SBATCH --job-name=dpav4_2-htune
+#SBATCH --job-name=dpav4_2-unreg-htune
 #SBATCH --partition=cocosys
 #SBATCH --account=cocosys
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-gpu=14
 #SBATCH --time=24:00:00
-#SBATCH --output=./outputs/dpav4_2/reg_htune/slurm_%A_%a.out
-#SBATCH --error=./outputs/dpav4_2/reg_htune/slurm_%A_%a.out
+#SBATCH --output=./outputs/dpav4_2/unreg_htune/slurm_%A_%a.out
+#SBATCH --error=./outputs/dpav4_2/unreg_htune/slurm_%A_%a.out
 #SBATCH --array=0-999%2
 
 source ~/.bashrc
 micromamba activate leakage-localization
 python -m experiments.train.supervised \
-    --dest ./outputs/dpav4_2/reg_htune \
+    --dest ./outputs/dpav4_2/unreg_htune \
     --config-file dpav4_2 \
-    --optuna-study-path ./outputs/dpav4_2/reg_htune/optuna_study.log \
+    --optuna-study-path ./outputs/dpav4_2/unreg_htune/optuna_study.log \
     --sampler qmc

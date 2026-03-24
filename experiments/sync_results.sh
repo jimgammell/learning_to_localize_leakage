385c325f9230e6b3f8457f00a5cb0cab55ea2194
Jim Gammell <jgammell@purdue.edu>
2026-03-24 18:39:02 -0400

    ...

--- experiments/sync_results.sh
+++ experiments/sync_results.sh
@@ -3,7 +3,6 @@
 # We are selectively streaming results according to the following criteria:
 #   1) Users should be able to selectively run individual experiments without having to run e.g. hyperparameter tuning first.
 #   2) We want users to be able to re-generate the important plots from our paper without training models.
-#   3) We will exclude ablations, model checkpoints from hyperparameter sweeps, etc. to keep download size manageable.
 
 src=$1
 dest=$2
@@ -13,8 +12,6 @@ rsync -av \
     --exclude "*.png" \
     --exclude "*.pdf" \
     --exclude "*ablation" \
-    --exclude "*hparam_sweep/trial_*" \
-    --exclude "*hparam_sweep/classifiers_pretraining" \
     --exclude "*all_sensitivity_analysis" \
     --exclude "*backup* \
     $src $dest
